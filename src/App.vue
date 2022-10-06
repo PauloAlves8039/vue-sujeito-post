f527de2fc85f1685558efb514cab6b83c2da84ac
PauloAlves8039 <pj38alves@gmail.com>
2022-10-06 20:22:18 -0300

    Criando o componente Header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-
+    <Header/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
-  
+  components:{
+    Header
+  }
 }
 </script>
+import Header from './components/Header';
 
 <style scoped>
-
+  #app{
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
 </style>
