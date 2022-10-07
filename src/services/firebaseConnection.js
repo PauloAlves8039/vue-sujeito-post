c7692417953c6a256dfab79ed3878ee5c78ec576
PauloAlves8039 <pj38alves@gmail.com>
2022-10-07 08:14:30 -0300

    Adicionando o firebase

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import 'firebase/firebase';
+import 'firebase/auth';
+
+let firebaseConfig = {
+  apiKey: "AIzaSyBQVqq89OLrozlHz-0fIHqb0TR2UTtAIEI",
+  authDomain: "sujeitopost-d64e5.firebaseapp.com",
+  projectId: "sujeitopost-d64e5",
+  storageBucket: "sujeitopost-d64e5.appspot.com",
+  messagingSenderId: "859883856583",
+  appId: "1:859883856583:web:60dd698ed54ab3628794e2",
+  measurementId: "G-0KFTHSSZGC",
+};
+
+if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
