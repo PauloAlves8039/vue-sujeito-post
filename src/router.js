ff89dd5a3cb52d9d36054976bfde013cbd20e071
PauloAlves8039 <pj38alves@gmail.com>
2022-10-08 10:01:17 -0300

    Aplicando configurações de rotas

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import firebase from './services/firebaseConnection';
+
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
@@ -14,24 +16,40 @@ const router = new Router({
         {
             path: '/',
             component: Home,
+            meta:{
+                requiresAuth: true,
+            }
         },
         {
             path: '/dashboard',
             component: Dashboard,
-        },
-        {
-            path: '/dashboard',
-            component: Dashboard,
+            meta:{
+                requiresAuth: true,
+            }
         },
         {
             path: '/perfil/:userid',
             component: Perfil,
+            props: true,
+            meta:{
+                requiresAuth: true,
+            }
         },
         {
             path: '/login',
             component: Login,
         },
     ]
+});
+
+router.beforeEach((to, from, next) => {
+    const requiresAuth = to.matched.some(x => x.meta.requiresAuth);
+
+    if (requiresAuth && !firebase.auth().currentUser) {
+        next('/login');
+    }else {
+        next();
+    }
 });
 
 export default router;
