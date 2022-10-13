bfc3b450610a414476d2b6224da48a3491eebf67
PauloAlves8039 <pj38alves@gmail.com>
2022-10-13 20:20:34 -0300

    Aplicando a busca do post

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,98 +1,128 @@
 <template>
   <div id="home">
-      
-      <div class="newpost">
-        <h2>Bem vindo de volta!</h2>
-        <span>Compartilhe seu dia</span>
-        <textarea
+    <div class="newpost">
+      <h2>Bem vindo de volta!</h2>
+      <span>Compartilhe seu dia</span>
+      <textarea
         placeholder="O que está fazendo hoje?"
         rows="15"
         v-model="input"
-        >
-        </textarea>
-        <button @click="craetePost">Compartilhar</button>
-      </div>
+      >
+      </textarea>
+      <button @click="craetePost">Compartilhar</button>
+    </div>
 
-      <div class="postarea">
-        <article class="post">
-          <h1>Matheus</h1>
-          <p>
-            Olá este é meu primeiro post aqui na plataforma sujeitoPost :)
-          </p>
+    <div class="postarea loading" v-if="loading">
+      <h2>Buscando posts...</h2>
+    </div>
 
-          <div class="action-post">
-            <button>20 curtidas</button>
-            <button>Veja post completo</button>
-          </div>
-        </article>
+    <div class="postarea" v-else>
+      <article class="post" v-for="post in posts" :key="post.id">
+        <h1>{{ post.autor }}</h1>
+        <p>
+          {{ post.content }}
+        </p>
+        <div class="action-post">
+          <button>
+            {{ post.likes === 0 ? "Curtir" : post.likes + " Curtidas" }}
+          </button>
+          <button>Veja post completo</button>
+        </div>
+      </article>
 
-        <article class="post">
-          <h1>Lucas Oliveira</h1>
-          <p>
-            Olá este é meu primeiro post aqui na plataforma sujeitoPost :)
-          </p>
+      <article class="post">
+        <h1>Lucas Oliveira</h1>
+        <p>Olá este é meu primeiro post aqui na plataforma sujeitoPost :)</p>
 
-          <div class="action-post">
-            <button>20 curtidas</button>
-            <button>Veja post completo</button>
-          </div>
-        </article>
-      </div>
-
+        <div class="action-post">
+          <button>20 curtidas</button>
+          <button>Veja post completo</button>
+        </div>
+      </article>
+    </div>
   </div>
 </template>
 
 <script>
-import firebase from '../services/firebaseConnection';
-
+import firebase from "../services/firebaseConnection";
 
 export default {
-  name: 'Home',
-  data(){
-    return{
-      input: '',
+  name: "Home",
+  data() {
+    return {
+      input: "",
       user: {},
-    }
+      loading: true,
+      posts: [],
+    };
   },
-  created(){
-    const user = localStorage.getItem('devpost');
+  async created() {
+    const user = localStorage.getItem("devpost");
     this.user = JSON.parse(user);
+
+    await firebase
+      .firestore()
+      .collection("posts")
+      .orderBy("created", "desc")
+      .onSnapshot((doc) => {
+        this.posts = [];
+
+        doc.forEach((item) => {
+          this.posts.push({
+            id: item.id,
+            autor: item.data().autor,
+            content: item.data().content,
+            likes: item.data().likes,
+            created: item.data().created,
+            userId: item.data().userId,
+          });
+        });
+        this.loading = false;
+        console.log(this.posts);
+      });
   },
-  methods:{
-    async craetePost(){
-      if(this.input === ''){
+  methods: {
+    async craetePost() {
+      if (this.input === "") {
         return;
       }
 
-      await firebase.firestore().collection('posts')
-      .add({
-        created: new Date(),
-        content: this.input,
-        autor: this.user.nome,
-        userId: this.user.uid,
-        likes: 0,
-      })
-      .then(()=> {
-        this.input = '';
-        console.log('POST CRIADO COM SUCESSO!');
-      })
-      .catch((error)=>{
-        console.log('Error ao criar o post: ' + error);
-      })
-
-    }
+      await firebase
+        .firestore()
+        .collection("posts")
+        .add({
+          created: new Date(),
+          content: this.input,
+          autor: this.user.nome,
+          userId: this.user.uid,
+          likes: 0,
+        })
+        .then(() => {
+          this.input = "";
+          console.log("POST CRIADO COM SUCESSO!");
+        })
+        .catch((error) => {
+          console.log("Error ao criar o post: " + error);
+        });
+    },
+  },
+  filters:{
+    postLength(valor){
+        if (valor.length < 200) { 
+            return valor 
+        }
+        return `${valor.substring(0, 200)}...`
+      }
   }
-
-}
+};
 </script>
 
 <style scoped>
- #home{
-   display: flex;
-   flex-direction: row;
-   margin: 25px;
- }
+#home {
+  display: flex;
+  flex-direction: row;
+  margin: 25px;
+}
 
- @import './home.css';
-
+@import "./home.css";
 </style>
